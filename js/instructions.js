a9aacc698825811a0d9552afe9a95aaf661b7539
Ditte <dian1001@stud.ek.dk>
2025-11-03 08:32:06 +0100

    svg

---
+++ js/instructions.js
@@ -0,0 +1,8 @@
+console.log("loaded instructions...");
+document.querySelector("#info1").addEventListener("click", infoF);
+document.querySelector("#info2").addEventListener("click", infoF);
+document.querySelector("#info3").addEventListener("click", infoF);
+
+function infoF() {
+  console.log("infoF er her");
+}
